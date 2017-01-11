ac846f31a29c6de8d2a7e6d069d97f5e5f87d7f8
ELHkokada <ELHkokada@users.noreply.github.com>
2017-01-11 10:17:40 -0500

    Create CustomStudent Example.sql

---
+++ CustomStudent Example.sql
@@ -0,0 +1,5 @@
+select * from CustomStudent where attributeID=606 
+--606 is just in here as a placeholder.  Put whatever ID you want here to look at CustomStudent
+
+select * from CampusAttribute where name like '%searchForAttribute%' 
+--to find a particular word in the attribute names, replace the 'searchForAttribute' text with the words you want to look for.
