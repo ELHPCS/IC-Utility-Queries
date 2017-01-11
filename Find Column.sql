7b129980115805de692061c08ef882e77f8e227f
ELHkokada <ELHkokada@users.noreply.github.com>
2017-01-11 10:20:41 -0500

    Update Find Column.sql

--- Find Column.sql
+++ Find Column.sql
@@ -1,3 +1,6 @@
+--This code will search all the tables in your SQL database and return any column that contains the string 'objectID'.
+--Replace 'objectID' with the string you want to search for.
+
 select o.name,c.name 
 from sys.columns c inner join sys.objects  o on c.object_id=o.object_id 
 and o.type = 'U'
