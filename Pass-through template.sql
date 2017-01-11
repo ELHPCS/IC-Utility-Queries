1ae46f22382c3eff6c66b39702f603e366e0641c
ELHkokada <ELHkokada@users.noreply.github.com>
2017-01-11 10:48:08 -0500

    Update Pass-through template.sql

--- Pass-through template.sql
+++ Pass-through template.sql
@@ -3,7 +3,7 @@ SELECT DISTINCT TOP 1000 student.personID,
  student.grade, student.studentNumber 
  FROM student WITH (NOEXPAND) 
  /***********************************/
-
+--Put any of your joins here.  This is "Box 1."
 
 
 
@@ -11,7 +11,7 @@ SELECT DISTINCT TOP 1000 student.personID,
  WHERE  student.calendarID = 37 AND ( 0 =2017 OR student.endYear = 2017) --replace 37, 2017, and 13 with the correct values for your pass-through query
  AND ( 0 = 13 OR student.structureID = 13) 
   /***********************************/
-
+--Put any additional where conditions here.  This is "Box 2."
 
 
 
